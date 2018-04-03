2e7c5371354a26c1ed5cdbf68066d9fae035280d
soloproyectos <gonzalo.chumillas@gmail.com>
2018-04-03 19:50:41 +0200

    update

--- src/app/views/contact/contact-view.ts
+++ src/app/views/contact/contact-view.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-view.scss']
 })
 export class ContactView {
+  env = environment;
 
+  constructor(private _sanitizer: DomSanitizer) { }
+
+  sanitize(url: string) {
+    return this._sanitizer.bypassSecurityTrustUrl(url);
+  }
 }
