76001bcf72f397d367cc946e1c91f10ccb84577f
soloproyectos <gonzalo.chumillas@gmail.com>
2018-04-03 20:31:42 +0200

    cssfix

--- src/app/views/software/software-view.ts
+++ src/app/views/software/software-view.ts
@@ -5,7 +5,11 @@ import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-software',
   templateUrl: './software-view.html',
-  styleUrls: ['./software-view.scss', './software-view-desktop.scss']
+  styleUrls: [
+    './software-view.scss',
+    './software-view-tablet.scss',
+    './software-view-desktop.scss'
+  ]
 })
 export class SoftwareView {
   env = environment;
