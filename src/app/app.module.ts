2622be0d987f81640e7f1e930ea0a8c51fba5205
soloproyectos <gonzalo.chumillas@gmail.com>
2018-04-03 16:36:37 +0200

    install ngx-scroll-to

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
 import { AppRoutingModule } from './app.routing-module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { SoftwareView } from './views/software/software-view';
   ],
   imports: [
     BrowserModule,
+    ScrollToModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
