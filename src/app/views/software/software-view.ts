d6e2d06905c2f05d448fdad76f3b2f44ef00a6bb
soloproyectos <gonzalo.chumillas@gmail.com>
2018-04-03 17:23:03 +0200

    update

--- src/app/views/software/software-view.ts
+++ src/app/views/software/software-view.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-software',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./software-view.scss']
 })
 export class SoftwareView {
+  env = environment;
 
+  constructor(private _sanitizer: DomSanitizer) { }
+
+  sanitize(url: string) {
+    return this._sanitizer.bypassSecurityTrustUrl(url);
+  }
 }
